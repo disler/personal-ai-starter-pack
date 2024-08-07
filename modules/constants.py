5ab166a10aabf1754b5fd8bd3377345c485b04ba
Timothy Johnson <jhnsntmthy@gmail.com>
2024-08-06 20:59:44 -0400

    added default of openai

--- modules/constants.py
+++ modules/constants.py
@@ -2,7 +2,7 @@ import os
 import toml
 
 def load_config():
-    config_name = os.environ.get("AI_CONFIG", "default")
+    config_name = os.environ.get("AI_CONFIG", "openai")
     config_path = os.path.join(os.path.dirname(__file__), f"../configs/{config_name}.toml")
     
     if not os.path.exists(config_path):
